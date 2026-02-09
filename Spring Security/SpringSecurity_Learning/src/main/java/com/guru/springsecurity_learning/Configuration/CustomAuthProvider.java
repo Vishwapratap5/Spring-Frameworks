30b3aecbec604df6d453a5b92d5008ecff38a50c
Vishwapratap5 <revandeguru@gmail.com>
2026-02-10 00:22:12 +0530

    Custom AuthenticationProvider

---
+++ Spring Security/SpringSecurity_Learning/src/main/java/com/guru/springsecurity_learning/Configuration/CustomAuthProvider.java
@@ -0,0 +1,40 @@
+package com.guru.springsecurity_learning.Configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthProvider implements AuthenticationProvider {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails user=userDetailsService.loadUserByUsername(username);
+
+        if(!passwordEncoder.matches(password,user.getPassword())){
+            throw new BadCredentialsException("Incorrect credentials");
+        }
+        return new  UsernamePasswordAuthenticationToken(user,null,user.getAuthorities());
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+}
